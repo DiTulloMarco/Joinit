5b78dda1a6061ea973c50c9b25f127e31e1e1aba
AlSatan666 <alelamb565@gmail.com>
2024-09-26 15:43:12 +0200

    Aggiunte le pagine di login e registrazione(frontend Registrazione 1-2)

---
+++ joinit-fe/src/app/login/page.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-md shadow-md w-80">
+        <h1 className="text-2xl font-bold text-center mb-6">Accedi</h1>
+        <form className="space-y-4">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="you@example.com"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="you_secure_password!"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="space-y-4">
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-gray-400 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Accedi
+            </button>
+          </div>
+        </form>
+        <div className="text-center mt-6">
+          <p className="text-sm text-gray-600">Non hai un account?</p>
+          <a href="/register" className="w-full flex justify-center py-2 px-4 border border-gray-400 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-2">
+            Registrati
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
