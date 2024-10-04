bf6a544c45c4e0f83c5a7ecd8dacd0b766208ffc
AlSatan666 <alelamb565@gmail.com>
2024-10-04 17:57:25 +0200

    ampliata la funzione search con varie categorie di ricerca(backend 20)

--- joinit/events/views.py
+++ joinit/events/views.py
@@ -30,27 +30,52 @@ class EventViewSet(ModelViewSet):
     
     @action(detail=False, methods=['get'])
     def search_events(self, request):
-        query = request.query_params.get('query', None)
+        filters = Q()  # Inizializza il filtro vuoto
 
-        if query:
-            events = Event.objects.filter(
-                Q(name__icontains=query) |
-                Q(description__icontains=query) |
-                Q(city__icontains=query)
-            ).order_by('-starting_ts')
-        else:
-            events = Event.objects.all().order_by('-starting_ts')
+        category = request.query_params.get('category', None)
+        name = request.query_params.get('name', None)
+        description = request.query_params.get('description', None)
+        city = request.query_params.get('city', None)
+        price_min = request.query_params.get('price_min', None)
+        price_max = request.query_params.get('price_max', None)
 
-    # Utilizza la paginazione
+        # Filtra per nome, se fornito
+        if name:
+            filters &= Q(name__icontains=name)
+
+        # Filtra per descrizione, se fornito
+        if description:
+            filters &= Q(description__icontains=description)
+    
+        # Filtra per città, se fornito
+        if city:
+            filters &= Q(city__icontains=city)
+
+        # Filtra per intervallo di prezzo, se forniti
+        if price_min and price_max:
+            filters &= Q(price__gte=price_min, price__lte=price_max)
+        elif price_min:
+            filters &= Q(price__gte=price_min)
+        elif price_max:
+            filters &= Q(price__lte=price_max)
+
+
+        # Add the category filter
+        if category:
+            filters &= Q(category=category)
+
+
+        # Apply the filters to the queryset
+        events = Event.objects.filter(filters).order_by('-starting_ts')
+
+        # Paginate the result if necessary
         page = self.paginate_queryset(events)
         if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+            serialized_objs = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serialized_objs.data)
 
-   
-   
-        serializer = self.get_serializer(events, many=True)
-        return Response(serializer.data)
+        serialized_objs = self.get_serializer(events, many=True)
+        return Response(serialized_objs.data)
     
 
     '''intendi la mia??? di che parli?ummm non so'''
